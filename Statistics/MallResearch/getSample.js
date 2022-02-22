f7b9db0ae17cd5939d80ab2bb27fb3ca2cb08258
ThumsRipa <thum.the.ripa@gmail.com>
2022-02-22 21:01:58 +0700

    MallUpdate Final

--- Statistics/MallResearch/getSample.js
+++ Statistics/MallResearch/getSample.js
@@ -15,11 +15,13 @@ function getRandomCitizen(){
     let individualArray = new Array(individualFileAmount); individualArray.fill(0); //Arrays to ensure that there are no repeated samples
 
     randomCitizensAge = [];
+    randomCitizensGender = [];
     randomCitizensHeight = [];
     randomCitizensWeight = [];
     randomCitizensPurpose = [];
     randomCitizensMoney = [];
-    var tableString = '<table id="sampleResultTable"><tr><th>ID</th><th>Age</th><th>Height (cm)</th><th>Weight (kg)</th><th>Purpose of Visit</th><th>Amount of Money Spent (Baht)</th>';
+    randomCitizensComplaint = [];
+    var tableString = '<table id="sampleResultTable"><tr><th>ID</th><th>Age</th><th>Gender</th><th>Height (cm)</th><th>Weight (kg)</th><th>Purpose of Visit</th><th>Amount of Money Spent (Baht)</th><th>Main Complaint</th></tr>';
     var sampleSize = 100; //sample size value, replace with text input from page if needed
 
     randomFile = Math.floor(Math.random() * fileAmount); //Random File
@@ -35,18 +37,22 @@ function getRandomCitizen(){
         individualArray[randomID] = 1;
 
         randomCitizensAge.push(allCitizens[randomFile]['Age'][randomID]); //For Analysis
+        randomCitizensGender.push(allCitizens[randomFile]['Gender'][randomID]); //For Analysis
         randomCitizensHeight.push(allCitizens[randomFile]['Height'][randomID]); //For Analysis
         randomCitizensWeight.push(allCitizens[randomFile]['Weight'][randomID]); //For Analysis
         randomCitizensPurpose.push(allCitizens[randomFile]['Purpose of Visit'][randomID]); //For Analysis
         randomCitizensMoney.push(allCitizens[randomFile]['Amount of Money Spent'][randomID]); //For Analysis
+        randomCitizensComplaint.push(allCitizens[randomFile]['Main Complaint'][randomID]); //For Analysis
 
         tableString += '<tr>';
         tableString += '<td>' + allCitizens[randomFile]['ID'][randomID] + '</td>';
         tableString += '<td>' + allCitizens[randomFile]['Age'][randomID] + '</td>';
+        tableString += '<td>' + allCitizens[randomFile]['Gender'][randomID] + '</td>';
         tableString += '<td>' + allCitizens[randomFile]['Height'][randomID] + '</td>';
         tableString += '<td>' + allCitizens[randomFile]['Weight'][randomID] + '</td>';
         tableString += '<td>' + allCitizens[randomFile]['Purpose of Visit'][randomID] + '</td>';
         tableString += '<td>' + allCitizens[randomFile]['Amount of Money Spent'][randomID] + '</td>';
+        tableString += '<td>' + allCitizens[randomFile]['Main Complaint'][randomID] + '</td>';
         tableString += '</tr>';
         //randomCitizens.push(allCitizens[randomFile]['ID'][randomID]) //get Citizen ID
     }
@@ -55,11 +61,27 @@ function getRandomCitizen(){
 
     /////////// Get and display analysis of data
     allPurposes = getPercentages(randomCitizensPurpose);
+    allGenders = getGenPercentages(randomCitizensGender);
+    allComplaints = getComPercentages(randomCitizensComplaint);
+
     var numericalString = '<table id="numericalResultTable">'
+    numericalString += '<tr class="numbers"><td colspan="12">General Information</td><tr>'
     numericalString += '<tr class="numbers"><td colspan="3">Average Age (Years):<br>Standard Deviation Age (Years):</td><td colspan="3">' + getAverage(randomCitizensAge) + '</td><td colspan="3">Average Money Spent (Baht):<br>Standard Deviation Money Spent (Baht):</td><td colspan="3">' + getAverage(randomCitizensMoney) + '</td></tr>';
-    numericalString += '<tr class="numbers"><td colspan="3">Average Height (cm):<br>Standard Deviation Height (cm):</td><td colspan="3">' + getAverage(randomCitizensHeight) + '</td><td colspan="3">Average Weight (kg)<br>Standard Deviation Weight (kg)</td><td colspan="3">' + getAverage(randomCitizensWeight) + '</td></tr>';
-    numericalString += '<tr class="percentages"><td colspan="2">Shopping %</td><td colspan="2">' + allPurposes[0] + '%</td><td colspan="2">Eating %</td><td colspan="2">' + allPurposes[1] + '%</td><td colspan="2">Socializing %</td><td colspan="2">' + allPurposes[2] + '%</td>'
-    numericalString += '<tr class="percentages"><td colspan="2">Movies %</td><td colspan="2">' + allPurposes[3] + '%</td><td colspan="2">Work-Related %</td><td colspan="2">' + allPurposes[4] + '%</td><td colspan="2">Others %</td><td colspan="2">' + allPurposes[5] + '%</td>'
+    numericalString += '<tr class="numbers"><td colspan="3">Average Height (cm):<br>Standard Deviation Height (cm):</td><td colspan="3">' + getAverage(randomCitizensHeight) + '</td><td colspan="3">Average Weight (kg):<br>Standard Deviation Weight (kg):</td><td colspan="3">' + getAverage(randomCitizensWeight) + '</td></tr>';
+    //End Age, Height, Weight, Money
+    //Start Gender
+    numericalString += '<tr class="numbers"><td colspan="3">Male:</td><td colspan="3">' + allGenders[0] + '%</td><td colspan="3">Female:</td><td colspan="3">' + allGenders[1] + '%</td></tr>';
+    //End Gender
+    //Start Reason to Shop
+    numericalString += '<tr class="percentages"><td colspan="12">Why did you come to the mall?</td><tr>'
+    numericalString += '<tr class="percentages"><td colspan="2">Shopping</td><td colspan="2">' + allPurposes[0] + '%</td><td colspan="2">Eating</td><td colspan="2">' + allPurposes[1] + '%</td><td colspan="2">Socializing</td><td colspan="2">' + allPurposes[2] + '%</td>'
+    numericalString += '<tr class="percentages"><td colspan="2">Movies</td><td colspan="2">' + allPurposes[3] + '%</td><td colspan="2">Work-Related</td><td colspan="2">' + allPurposes[4] + '%</td><td colspan="2">Others</td><td colspan="2">' + allPurposes[5] + '%</td>'
+    //End Reason to Shop
+    //Start Complaints
+    numericalString += '<tr class="numbers"><td colspan="12">What is your biggest complaint about the mall?</td><tr>'
+    numericalString += '<tr class="numbers"><td colspan="3">Limited Parking Space:</td><td colspan="3">' + allComplaints[0] + '%</td><td colspan="3">Limited Food Choice:</td><td colspan="3">' + allComplaints[1] + '%</td></tr>';
+    numericalString += '<tr class="numbers"><td colspan="3">Lack of Good Hangout Seating:</td><td colspan="3">' + allComplaints[2] + '%</td><td colspan="3">Lack of Good Directions:</td><td colspan="3">' + allComplaints[3] + '%</td></tr>';
+
     numericalString += '</table><br><br>';
     document.getElementById("numericalResult").innerHTML = numericalString;
     ///////////
@@ -100,6 +122,40 @@ function getPercentages(arrayHere){
             allCounts[2]++;
         } else {
             allCounts[0]++; //Shopping
+        }
+    }
+
+    return allCounts;
+} //Get percentages of each reason to visit ******* Will need to change if sample size is NOT 100
+
+function getGenPercentages(arrayHere){
+    allCounts = [0,0] //[Shopping, Eating, Socializing, Movies, Work, Others]
+
+    for(loop = 0; loop < arrayHere.length; loop++){
+        if(arrayHere[loop][0]=='M'){ //Male
+            allCounts[0]++;
+            continue
+        } else {
+            allCounts[1]++; //Female
+        }
+    }
+
+    return allCounts;
+} //Get percentages of each reason to visit ******* Will need to change if sample size is NOT 100
+
+function getComPercentages(arrayHere){
+    allCounts = [0,0,0,0] //[Parking, Food, Hangout, Directions]
+
+    for(loop = 0; loop < arrayHere.length; loop++){
+        if(arrayHere[loop]=='Limited Parking Space'){ //Parking
+            allCounts[0]++;
+            continue
+        } else if (arrayHere[loop]=='Limited Food Choice'){ //Eating
+            allCounts[1]++;
+        } else if (arrayHere[loop]=='Lack of Good Hangout Seating'){ //Hangout
+            allCounts[2]++;
+        } else {
+            allCounts[3]++; //Directions
         }
     }
 
